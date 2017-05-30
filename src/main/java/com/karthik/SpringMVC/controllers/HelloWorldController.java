3700dfc5a79b1a81a0b94cf467ae824e06c02635
Karthik <Karthik@DESKTOP-B90MR2O>
2017-05-31 00:33:49 +0530

    First Commit

---
+++ src/main/java/com/karthik/SpringMVC/controllers/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.karthik.SpringMVC.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+	
+	@RequestMapping("/hello")
+	public ModelAndView helloWorld(){
+		
+		String message = "Hello Karthik... Congrats";
+		
+		return new ModelAndView("hellopage","message",message) ;
+		
+	}
+
+}
